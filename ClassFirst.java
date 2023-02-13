941cba13bcfc8915865998f987e23e4831957664
jaymini patel <jaymini_bms@yahoo.co.uk>
2023-02-13 21:50:42 +0000

    my firstjava

---
+++ ClassFirst.java
@@ -0,0 +1,20 @@
+package packag1;
+
+public class ClassFirst {
+    public static void main(String[] args) {
+        System.out.println("i am jaymini");
+        System.out.println("i am a quick learner");
+        // a single line comment
+        /* a multi -line comments
+        i can wright */
+        /** documentation comments like to wright like this
+         * so every one can understand it */
+
+//I finish my project
+        /* I can still do more
+        which other can see
+        very easy to remember*/
+
+
+    }
+}
